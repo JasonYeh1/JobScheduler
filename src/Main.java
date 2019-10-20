6d58fb2a4e1300281af8f7c6d524d10e93fe0ec7
unknown <jasonyeh@cpp.edu>
2019-10-19 20:38:48 -0700

    Created pseudocode

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,69 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String args[]) {
         System.out.println("Hello World");
     }
 
-    FCFS(TextFile input) {
-        
-    }
+//    FCFS(TextFile input) {
+//        while(input.hasNext()) {
+//            String currentJob = input.nextString();
+//            int burstTime = input.nextInt();
+//            for(int i = 0; i < burstTime; i++) {
+//                process(currentJob);
+//            }
+//        }
+//    }
+//
+//    SJF(TextFile input) {
+//        HashMap<String, Integer> map = new HashMap<>();
+//        while(input.hasNext()) {
+//            String currentJob = input.nextString();
+//            int burstTime = input.nextInt();
+//            map.put(currentJob, burstTime);
+//        }
+//        Collections.sortByValue(map);
+//
+//        for(entry : map) {
+//            String currentJob = entry.getKey();
+//            int burstTime = entry.getValue();
+//            for(int i = 0; i < burstTime; i++) {
+//                process(currentJob);
+//            }
+//        }
+//    }
+//
+//    RR(TextFile input, int timeSlice) {
+//        int finishedJobs = 0;
+//        HashMap<String, Integer> map = new HashMap<>();
+//        while(input.hasNext()) {
+//            String currentJob = input.nextString();
+//            int burstTime = input.nextInt(0);
+//            map.put(currentJob, burstTime);
+//        }
+//        while(finishedJobs < map.size()) {
+//            for(entry : map) {
+//                String currentJob = entry.getKey();
+//                int timeBurst = entry.getValue();
+//                if(timeBurst > timeSlice) {
+//                    for(int i = 0; i < timeSlice; i++) {
+//                        process(currentJob);
+//                    }
+//                    map.put(currentJob, timeBurst - timeSlice);
+//                } else if(timeBurst == timeSlice) {
+//                    for(int i = 0; i < timeSlice; i++) {
+//                        process(currentJob);
+//                    }
+//                    finishedJobs++;
+//                    map.put(currentJob, timeBurst - timeSlice);
+//                } else {
+//                    for(int i = 0; i < entry.getValue(); i++) {
+//                        process(currentJob);
+//                    }
+//                    finishedJobs++;
+//                    map.put(currentJob, 0);
+//                }
+//            }
+//        }
+//    }
 }
